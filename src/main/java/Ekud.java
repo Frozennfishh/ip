5c57a5a594d5473bf1bcd83c6e64c61bc684b827
Frozennfishh <93506857+Frozennfishh@users.noreply.github.com>
2025-01-23 10:11:18 +0800

    Add Ekud.java

---
+++ src/main/java/Ekud.java
@@ -0,0 +1,15 @@
+public class Ekud {
+    public static void main(String[] args) {
+        String logo = "\nEEEEE K  KK U   U DDD   !\n"
+                    + "E     KKK   U   U D  D  !\n"
+                    + "EEEEE K     U   U D   D !\n"
+                    + "E     KKK   UU UU D  D   \n"
+                    + "EEEEE K  KK  UUU  DDD   !\n";
+        System.out.println("____________________________\n");
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("____________________________\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________\n");
+    }
+}
