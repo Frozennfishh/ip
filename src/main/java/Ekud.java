edeaf6df3eba0dc41f74e6bfb0ae9772510fef03
Frozennfishh <93506857+Frozennfishh@users.noreply.github.com>
2025-01-23 10:57:19 +0800

    Added list function

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,24 +1,36 @@
+import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Ekud {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
         intro();
 
         while (true) {
             String input = scanner.nextLine();
             if (Objects.equals(input, "bye")) {
                 break;
+            } else if (Objects.equals(input, "list")) {
+                if (list.isEmpty()) {
+                    System.out.println("List is empty!");
+                } else {
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i+1 + ". " + list.get(i));
+                    }
+                }
+                buffer();
             } else {
-                System.out.println(input + "\n");
-                System.out.println("ฅ^•ﻌ•^ฅ");
-                System.out.println("____________________________\n");
+                System.out.println("added: " + input);
+                list.add(input);
+                buffer();
             }
         }
-
         goodbye();
     }
 
@@ -35,6 +47,11 @@ public class Ekud {
                 "|、˜〵          \n" +
                 "じしˍ,)ノ\n");
         System.out.println("What can I do for you?\n");
+        System.out.println("____________________________\n");
+    }
+
+    public static void buffer() {
+        System.out.println("ฅ^•ﻌ•^ฅ");
         System.out.println("____________________________\n");
     }
 
