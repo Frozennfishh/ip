fcaca914bfee59f680655b53a105fcedab419fa5
Frozennfishh <93506857+Frozennfishh@users.noreply.github.com>
2025-01-23 10:35:21 +0800

    Added echo

--- src/main/java/Ekud.java
+++ src/main/java/Ekud.java
@@ -1,16 +1,25 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Ekud {
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         intro();
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (Objects.equals(input, "bye")) {
+                break;
+            } else {
+                System.out.println(input + "\n");
+                System.out.println("ฅ^•ﻌ•^ฅ");
+                System.out.println("____________________________\n");
+            }
+        }
+
         goodbye();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out));
-
-        reader.close();
-        writer.close();
     }
 
     public static void intro() {
@@ -18,9 +27,13 @@ public class Ekud {
                 + "E     KKK   U   U D  D  !\n"
                 + "EEEEE K     U   U D   D !\n"
                 + "E     KKK   UU UU D  D   \n"
-                + "EEEEE K  KK  UUU  DDD   !\n";
+                + "EEEEE K  KK  UUU  DDD   !";
         System.out.println("____________________________\n");
         System.out.println("Hello! I'm\n" + logo);
+        System.out.println("╱|、\n" +
+                "(^ˎ ^7  \n" +
+                "|、˜〵          \n" +
+                "じしˍ,)ノ\n");
         System.out.println("What can I do for you?\n");
         System.out.println("____________________________\n");
     }
