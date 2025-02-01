bc2db9952272c05f776fb83a009ae5730185de09
Frozennfishh <leonard.ong0108@gmail.com>
2025-02-01 21:34:29 +0800

    Redoing branching

---
+++ src/main/java/FileClass.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class FileClass {
+
+    public static void main(String[] args) {
+        File f = new File("data/list.txt");
+        System.out.println("full path: " + f.getAbsolutePath());
+        System.out.println("file exists?: " + f.exists());
+        System.out.println("is Directory?: " + f.isDirectory());
+    }
+
+}
