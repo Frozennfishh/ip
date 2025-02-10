2204851359e97b4eb0659870bbac82555c588146
Frozennfishh <93506857+Frozennfishh@users.noreply.github.com>
2025-02-10 15:59:42 +0800

    Revert "GUI reply box: change to display color based on some commands only"
    This reverts commit 3aaee23e50c61790952ad575de9d7ca6e65baa80.

--- src/main/java/ekud/gui/DialogBox.java
+++ src/main/java/ekud/gui/DialogBox.java
@@ -77,28 +77,14 @@ public class DialogBox extends HBox {
 
     private void changeDialogStyle(String commandType) {
         switch(commandType) {
-            case "ClearCommand":
-                dialog.getStyleClass().add("clear-label");
+            case "TodoCommand":
+                dialog.getStyleClass().add("todo-label");
                 break;
-            case "DeadlineCommand", "EventCommand", "TodoCommand":
-                dialog.getStyleClass().add("add-label");
+            case "DeadlineCommand":
+                dialog.getStyleClass().add("deadline-label");
                 break;
             case "DeleteCommand":
                 dialog.getStyleClass().add("delete-label");
-                break;
-            case "DueCommand", "FindCommand", "ListCommand":
-                dialog.getStyleClass().add("list-label");
-                break;
-            case "MarkCommand":
-                dialog.getStyleClass().add("mark-label");
-                break;
-            case "UnknownCommand":
-                dialog.getStyleClass().add("unknown-label");
-                break;
-            case "UnmarkCommand":
-                dialog.getStyleClass().add("unmark-label");
-                break;
-            case "ExitCommand":
                 break;
             default:
                 // Do nothing
