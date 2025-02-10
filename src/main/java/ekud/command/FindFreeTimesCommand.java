f9b7bf50de0879237e9980ac5c3d0c5373a232bf
Frozennfishh <leonard.ong0108@gmail.com>
2025-02-11 05:05:10 +0800

    FindFreeTimesCommand: able to process and sort events without displaying proper free time
    Method to process input has not yet been implemented
    
    *FindFreeTimesCommand class: added full method to find free time
    given amount of time and list of events with start and end time
    *Need to implement: regard and disregard tasks on event list
    depending on if the event has been done or not

--- src/main/java/ekud/command/FindFreeTimesCommand.java
+++ src/main/java/ekud/command/FindFreeTimesCommand.java
@@ -1,22 +1,22 @@
 package ekud.command;
 
-
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 
 import ekud.memory.Storage;
 import ekud.memory.TaskList;
 import ekud.parser.Parser;
 import ekud.tasks.Event;
-import ekud.tasks.Task;
 import ekud.ui.Ui;
 
 public class FindFreeTimesCommand extends Command {
-    private Integer hours;
-    private Integer minutes;
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    private String hoursString;
+    private String minuteString;
+    private int minutes = 0;
     private LocalDateTime current;
     public FindFreeTimesCommand(String input) {
         super(input);
@@ -24,27 +24,57 @@ public class FindFreeTimesCommand extends Command {
         if (this.getInput() != null) {
             String[] temp = input.split("/for ", 2);
             String[] temp2 = temp.length > 1 ? temp[1].split(":", 2) : null;
-            String hoursString = temp2 == null || temp2.length <= 1 ? null : temp2[0];
-            String minuteString = temp2 != null && temp2.length > 1 ? temp2[1] : null;
-            this.hours = hoursString != null && Parser.isInteger(hoursString)
-                    ? Integer.parseInt(hoursString) : null;
-            this.minutes = minuteString != null && Parser.isInteger(minuteString)
-                    ? Integer.parseInt(minuteString) : null;
+            this.hoursString = temp2 == null || temp2.length <= 1 ? null : temp2[0];
+            this.minuteString = temp2 != null && temp2.length > 1 ? temp2[1] : null;
+            this.minutes += hoursString != null && Parser.isInteger(hoursString)
+                    ? Integer.parseInt(hoursString) * 60 : 0;
+            this.minutes += minuteString != null && Parser.isInteger(minuteString)
+                    ? Integer.parseInt(minuteString) : 0;
         }
         current = LocalDateTime.now();
+        System.out.println(hoursString);
+        System.out.println(minuteString);
+        System.out.println(minutes);
     }
 
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
         super.execute(tasks, ui, storage);
-        if (hours == null) {
-            return "Hours given is invalid, try again! (Format: HH:MM)";
+        if (hoursString == null || !Parser.isInteger(hoursString)) {
+            return "Time given is invalid, try again! (Format: HH:MM)";
         }
-        if (minutes == null) {
-            return "Minutes given is invalid, try again! (Format: HH:MM)";
+        if (minuteString == null || !Parser.isInteger(minuteString)) {
+            return "Time given is invalid, try again! (Format: HH:MM)";
         }
         ArrayList<Event> eventList = tasks.getAllEvents();
         eventList.sort(Comparator.comparing(Event::getStart));
-        return null;
+        LocalDateTime startFreeTime = current;
+        LocalDateTime endFreeTime = null;
+        for (Event event : eventList) {
+            if (event.getStart().isBefore(startFreeTime)) {
+                startFreeTime = event.getEnd();
+                endFreeTime = null;
+                continue;
+            } else {
+                endFreeTime = event.getStart();
+                if (startFreeTime.until(endFreeTime.plusMinutes(1), ChronoUnit.MINUTES) >= minutes) {
+                    System.out.println("End Time:" + endFreeTime);
+                    break;
+                } else {
+                    startFreeTime = event.getEnd();
+                    endFreeTime = null;
+                }
+            }
+            System.out.println(startFreeTime);
+            System.out.println(endFreeTime);
+        }
+        if (startFreeTime == current && endFreeTime == null) {
+            return "You're free all the way to infinity!";
+        } else if (endFreeTime == null) {
+            return "You will only be free after " + startFreeTime.format(dateTimeFormatter);
+        } else {
+            return "You will be free from " + startFreeTime.format(dateTimeFormatter)
+                    + " to " + endFreeTime.format(dateTimeFormatter);
+        }
     }
 }
